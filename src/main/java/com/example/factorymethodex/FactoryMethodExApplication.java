95a2c840a7c46e2e18cfd1d42615a90f24b5c4ef
jinny-do <sonmojiri7@gmail.com>
2026-07-13 18:09:24 +0900

    factory method register

---
+++ src/main/java/com/example/factorymethodex/FactoryMethodExApplication.java
@@ -0,0 +1,13 @@
+package com.example.factorymethodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryMethodExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryMethodExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/factorymethodex/FactoryMethodExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.factorymethodex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryMethodExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
